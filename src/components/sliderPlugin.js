d2a40ac7040aba924c8cd9121fc995b04314cfe0
KnyazevaElena <hop13100@mail.ru>
2019-11-15 19:14:43 +0300

    change path

--- src/components/sliderPlugin.js
+++ src/components/sliderPlugin.js
@@ -6,7 +6,7 @@ import Carousel, {Modal, ModalGateway} from "react-images";
 /* Слайдер с фото */
 const img = [
     {id: 1, src: '/../images/img2.png', alt: "Картинка 1"},
-    {id: 2, src: '/../images/img3.jpg', alt: "Картинка 2"},
+    {id: 2, src: 'https://knyazeva.github.io/plugins-test-react/images/img3.jpg', alt: "Картинка 2"},
     {id: 3, src: '/../images/img5.jpg', alt: "Картинка 3"},
     {id: 4, src: '/../images/img6.jpg', alt: "Картинка 4"},
 ];
