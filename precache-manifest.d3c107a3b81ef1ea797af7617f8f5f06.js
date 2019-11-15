be616e5f74fe1a6b519f0a27b019f68ab93d5ae8
KnyazevaElena <hop13100@mail.ru>
2019-11-15 19:04:46 +0300

    Updates

---
+++ precache-manifest.d3c107a3b81ef1ea797af7617f8f5f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62e9f7090803bb5c371444ca3708a44",
+    "url": "/plugins-test-react/index.html"
+  },
+  {
+    "revision": "a870f8fb242889203bf7",
+    "url": "/plugins-test-react/static/css/2.8503fc2b.chunk.css"
+  },
+  {
+    "revision": "b7e7d46bdf986a0d0be6",
+    "url": "/plugins-test-react/static/css/main.f3cd6f48.chunk.css"
+  },
+  {
+    "revision": "a870f8fb242889203bf7",
+    "url": "/plugins-test-react/static/js/2.48c2e751.chunk.js"
+  },
+  {
+    "revision": "b7e7d46bdf986a0d0be6",
+    "url": "/plugins-test-react/static/js/main.a0463bba.chunk.js"
+  },
+  {
+    "revision": "47c5c3fe73474b919ab6",
+    "url": "/plugins-test-react/static/js/runtime-main.d40743e7.js"
+  }
+]);
